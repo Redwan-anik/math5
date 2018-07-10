3c3c33ef8b957ff4bdc37dabbf9b1a98c5931da8
Md.Redwanul haque <32310502+Redwan-anik@users.noreply.github.com>
2018-07-11 05:10:42 +0600

    Add files via upload

---
+++ Untitled3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+   double A;
+   double B;
+    double C;
+   double MEDIA;
+   scanf("%lf",&A);
+   scanf("%lf",&B);
+   scanf("%lf",&C);
+   A=A*2;
+   B=B*3;
+   C=C*5;
+
+
+   MEDIA=(A+B+C)/10;
+   printf("MEDIA = %.1lf\n",MEDIA);
+
+
+
+
+
+
+}
